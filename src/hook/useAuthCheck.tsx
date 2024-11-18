1999fa087277ecc62047a2f0c1d051dad5754828
arsimsejdiu <arsim.sejdiu@bik-air.com>
2024-11-18 21:44:24 +0100

    Meme creator

--- src/hook/useAuthCheck.tsx
+++ src/hook/useAuthCheck.tsx
@@ -5,7 +5,7 @@ const useAuthCheck = () => {
   useEffect(() => {
     const token = localStorage.getItem("auth-token");
     if (!token) {
-        window.location.href = "/";
+        window.location.href = "/login";
     }
   }, []);
 };
